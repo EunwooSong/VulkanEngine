de5f580c3ee67047572eb146f63fef9d6243e4d0
eunwoosong <songe00348@gmail.com>
2020-11-07 23:52:52 +0900

    Initialize CMake
    Vulkan, GLM, GLI, etc . . .

---
+++ Engine/cmake-build-debug/CMakeFiles/main.dir/DependInfo.cmake
@@ -0,0 +1,29 @@
+# The set of languages for which implicit dependencies are needed:
+set(CMAKE_DEPENDS_LANGUAGES
+  "CXX"
+  )
+# The set of files for implicit dependencies of each language:
+set(CMAKE_DEPENDS_CHECK_CXX
+  "C:/Users/EunwooSong/Desktop/Git/Vulkan/VulkanEngine/Engine/src/main.cpp" "C:/Users/EunwooSong/Desktop/Git/Vulkan/VulkanEngine/Engine/cmake-build-debug/CMakeFiles/main.dir/src/main.cpp.obj"
+  )
+set(CMAKE_CXX_COMPILER_ID "MSVC")
+
+# Preprocessor definitions for this target.
+set(CMAKE_TARGET_DEFINITIONS_CXX
+  "GLFW_DLL"
+  )
+
+# The include file search paths:
+set(CMAKE_CXX_TARGET_INCLUDE_PATH
+  "../src"
+  "C:/VulkanSDK/1.2.148.1/Include"
+  "C:/vcpkg/installed/x86-windows/include"
+  "C:/vcpkg/installed/x86-windows/include/AL"
+  )
+
+# Targets to which this target links.
+set(CMAKE_TARGET_LINKED_INFO_FILES
+  )
+
+# Fortran module output directory.
+set(CMAKE_Fortran_TARGET_MODULE_DIR "")
